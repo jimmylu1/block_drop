4335254cc3beaa2ee48146fe5660a99895e1d4fd
jimmylu1 <jimmy88lu@gmail.com>
2019-08-19 16:51:19 -0700

    add background and set tetris stage

--- react-tetris/src/components/Tetris.js
+++ react-tetris/src/components/Tetris.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import { createStage } from "../gameHelpers.js";
+import { StyledTetrisWrapper, StyledTetris } from "./styles/StyledTetris";
 
 import Stage from "./Stage";
 import Display from "./Display";
@@ -8,17 +9,19 @@ import StartButton from "./StartButton";
 
 const Tetris = () => {
   return (
-    <div>
-      <Stage stage={createStage()} />
-      <aside>
-        <div>
-          <Display text="Score" />
-          <Display text="Rows" />
-          <Display text="Levels" />
-        </div>
-        <StartButton />
-      </aside>
-    </div>
+    <StyledTetrisWrapper>
+      <StyledTetris>
+        <Stage stage={createStage()} />
+        <aside>
+          <div>
+            <Display text="Score" />
+            <Display text="Rows" />
+            <Display text="Levels" />
+          </div>
+          <StartButton />
+        </aside>
+      </StyledTetris>
+    </StyledTetrisWrapper>
   );
 };
 
