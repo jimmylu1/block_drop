4bba3d2d2876ab199b392cbde1b46815621b2733
jimmylu1 <jimmy88lu@gmail.com>
2019-08-19 20:21:19 -0700

    create update player functions

--- react-tetris/src/components/Tetris.js
+++ react-tetris/src/components/Tetris.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 
+import { createStage } from "../gameHelpers";
+
 //styled components
 import { StyledTetrisWrapper, StyledTetris } from "./styles/StyledTetris";
 
@@ -20,8 +22,42 @@ const Tetris = () => {
   const [stage, setStage] = useStage(player);
 
   console.log("re-render");
+
+  const movePlayer = dir => {
+    updatePlayerPos({ x: dir, y: 0 });
+  };
+
+  const startGame = () => {
+    //reset everything
+    setStage(createStage());
+    resetPlayer();
+  };
+
+  const drop = () => {
+    updatePlayerPos({ x: 0, y: 1, collided: false });
+  };
+
+  const dropPlayer = () => {
+    drop();
+  };
+
+  const move = ({ keyCode }) => {
+    if (!gameOver) {
+      //move left
+      if (keyCode === 37) {
+        movePlayer(-1);
+        //move right
+      } else if (keyCode === 39) {
+        movePlayer(1);
+        //move down
+      } else if (keyCode === 40) {
+        dropPlayer();
+      }
+    }
+  };
+
   return (
-    <StyledTetrisWrapper>
+    <StyledTetrisWrapper role="buttom" tabIndex="0" onKeyDown={e => move(e)}>
       <StyledTetris>
         <Stage stage={stage} />
         <aside>
@@ -34,7 +70,7 @@ const Tetris = () => {
               <Display text="Level" />
             </div>
           )}
-          <StartButton />
+          <StartButton onClick={startGame} />
         </aside>
       </StyledTetris>
     </StyledTetrisWrapper>
