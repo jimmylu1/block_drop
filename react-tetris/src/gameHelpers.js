d9d2fdbac4edca31f7b118fc97e181ae54c8197e
jimmylu1 <jimmy88lu@gmail.com>
2019-08-20 14:49:18 -0700

    check collision

--- react-tetris/src/gameHelpers.js
+++ react-tetris/src/gameHelpers.js
@@ -5,3 +5,26 @@ export const createStage = () =>
   Array.from(Array(STAGE_HEIGHT), () =>
     new Array(STAGE_WIDTH).fill([0, "clear"])
   );
+
+
+export const checkCollision = (player, stage, {x: moveX, y: moveY}) => {
+  for(let y = 0; y < player.tetromino.length; y++) {
+    //rows are same length
+    for(let x = 0; x < player.tetromino[y].length; x++) {
+      //check if in tetromino cell
+      if(player.tetromino[y][x] !== 0) {
+        if(
+        //check movement in game area height
+        //should not go through bottom of the game area 
+        !stage[y + player.pos.y + moveY] || 
+        //check that move is inside game area width
+        !stage[y + player.pox.y + moveY][x + player.pos.x + moveX] || 
+        //check that cell we're moving to isn't set to clear
+          stage[y + player.pos.y + moveY][x + player.pos.x + moveX][1] !== 'clear'
+        ) {
+          return true;
+        }
+      }
+    }
+  }
+}
